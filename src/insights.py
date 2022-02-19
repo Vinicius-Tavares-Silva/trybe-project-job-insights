43ed6ea1d9f853b317776f5cac136b728e7fc1ec
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-02-19 13:13:44 -0300

    Requisito 7 - filter_by_industry

--- src/insights.py
+++ src/insights.py
@@ -78,7 +78,10 @@ def filter_by_industry(jobs, industry):
     list
         List of jobs with provided industry
     """
-    return []
+    filtred_jobs = list(
+        filter(lambda job: job['industry'] == industry, jobs)
+    )
+    return filtred_jobs
 
 
 def get_max_salary(path):
